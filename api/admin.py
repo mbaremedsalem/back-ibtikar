9bc34978b9f014935dcae88b00272e32de549f34
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2023-10-25 08:38:39 -0700

    create models

---
+++ api/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from .models import UserIbtikar,Etudiant,Manager,Cours,Video,Transaction
+# Register your models here.
+
+class UserAdminConfig(admin.ModelAdmin):
+    model = UserIbtikar
+    search_fields = ('email', 'name', 'phone','prenom',)
+    list_filter = ('email', 'name', 'phone', 'is_active', 'is_staff')
+    ordering = ('name',)  # Update the ordering field here
+    list_display = ('phone','email', 'name', 'prenom','is_superuser',
+                    'is_active', 'is_staff', 'is_blocked', 'password',)
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'prenom','phone','role',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_blocked')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name','prenom','phone', 'is_active', 'is_staff', 'is_blocked')
+            }
+         ),
+    )
+
+class UserManager(admin.ModelAdmin):
+    model = UserIbtikar
+    search_fields = ('email', 'name', 'phone','prenom',)
+    list_filter = ('email', 'name', 'phone', 'is_active', 'is_staff')
+    ordering = ('name',)  # Update the ordering field here
+    list_display = ('phone','prenom','email', 'name', 'is_superuser','prenom',
+                    'is_active', 'is_staff', 'is_blocked', 'password',)
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'phone','image','role','prenom')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_blocked')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'prenom','phone', 'is_active', 'is_staff', 'is_blocked')
+            }
+         ),
+    )
+
+admin.site.register(UserIbtikar, UserAdminConfig)
+admin.site.register(Etudiant, UserManager)
+admin.site.register(Manager, UserManager)
+admin.site.register(Cours)
+admin.site.register(Video)
+admin.site.register(Transaction)
---
+++ api/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from .manager import UserManager 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+# Create your models here.
+
+Role=(
+    ('Manager', 'Manager'),
+    ('Etudiant', 'Etudiant'),
+)  
+
+class UserIbtikar(AbstractBaseUser,PermissionsMixin):
+    name = models.CharField(max_length=50,blank=True)
+    prenom = models.CharField(max_length=50,blank=True)
+    phone = models.CharField(max_length=16,unique=True)
+    email = models.EmailField(max_length=50,blank=True)
+    date_naissance = models.DateTimeField(null=True)
+    address = models.CharField(max_length=200,)
+    is_active = models.BooleanField(default=True)
+    verified = models.BooleanField(default=False)
+    restricted = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    role= models.CharField(max_length=30, choices=Role, default='Manager')
+    is_superuser = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self): 
+        return self.name 
+    
+def image_uoload_profile(instance,filname):
+    imagename,extention =  filname.split(".")
+    return "user/%s.%s"%(instance.id,extention)    
+
+class Etudiant(UserIbtikar):
+    image=models.ImageField(upload_to=image_uoload_profile ,null=True)
+    def __str__(self): 
+        return self.phone 
+        
+#--------manager -----------
+class Manager(UserIbtikar):
+    image=models.ImageField(upload_to=image_uoload_profile ,null=True)   
+
+    def __str__(self): 
+        return self.phone     
+    
+class Cours(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    prix = models.DecimalField(max_digits=8, decimal_places=2)
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.titre
+    
+
+class Video(models.Model):
+    titre = models.CharField(max_length=100)
+    fichier_video = models.FileField(upload_to='videos/')
+    date_upload = models.DateTimeField(auto_now_add=True)
+    cours = models.ForeignKey(Cours, on_delete=models.CASCADE, related_name='videos')
+    def __str__(self):
+        return self.titre   
+    
+class Transaction(models.Model):
+    etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE)
+    cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    numTransaction = models.CharField(max_length=200,blank=True)
+    amount = models.FloatField()
+    def __str__(self):
+        return f"Transaction de {self.etudiant.nom} pour {self.cours.titre}"
